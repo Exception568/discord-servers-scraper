c80fbeb2bc7b73c2e4b4941ba9ebc8ffaea3ff3f
Exception568 <hashcatx568@gmail.com>
2022-08-16 20:00:26 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -4,6 +4,6 @@ r = requests.get('https://www.reddit.com/domain/discord.gg/new.json', headers={'
 json = r.json()
 servers = {}
 for url in json['data']['children']:
-    servers[url['data']['url']] = {'link': url['data']['url']}
+    servers[url['data']['url']] = {'invite': url['data']['url']}
 with open('servers.json', 'w') as serversf:
     json.dump(servers, serversf)
